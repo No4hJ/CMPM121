81252b54e9bd7388646761b0f87500aca677ee38
Noah Jiang <nujiang@ucsc.edu>
2022-11-09 23:52:49 -0800

    light switch

---
+++ classroom/Assets/Light_Switch.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Light_Switch : MonoBehaviour
+{
+        public List<Light> Lights;
+        public VisualElement frame;
+        private Button button;
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        var uiDocument = GetComponent<UIDocument>();
+        var rootVisualElement = uiDocument.rootVisualElement;
+        frame = rootVisualElement.Q<VisualElement>("Frame");
+        button = frame.Q<Button>("Light_Button");
+        button.RegisterCallback<ClickEvent>(ev => LightChange());
+    }
+
+    int click = 0;
+    private void LightChange()
+    {
+        if(click == 0){
+            Lights.ForEach(light => light.enabled = true);
+            click ++;
+        }
+        else if (click == 1){
+            Lights.ForEach(light => light.enabled = false);
+            click = 0;
+        }
+
+        Debug.Log(click);
+    }
+
+}
+
